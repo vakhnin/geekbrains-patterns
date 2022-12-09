a55c48f82eaa70db7aeb43946196974e97316c73
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-12-09 11:17:03 +0300

    Отдача сервером страниц по URL

--- framework/main.py
+++ framework/main.py
@@ -1,5 +1,20 @@
 class Framework:
 
     def __call__(self, environ, start_response):
-        start_response('200 Ok', [('Content-Type', 'text/html')])
-        return ['Answer'.encode('utf-8')]
+        path = environ['PATH_INFO']
+
+        # проверяем наличие слеша в конце URL
+        if not path.endswith('/'):
+            path += '/'
+
+        code = '200 Ok'
+        if path == '/':
+            answer = 'Index page'
+        elif path == '/about/':
+            answer = 'About page'
+        else:
+            code = '404 Not found'
+            answer = '404 Not found'
+
+        start_response(code, [('Content-Type', 'text/html')])
+        return [answer.encode('utf-8')]
