f78f6915726b4c648a46ec3b69dcc8bc97958bc9
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-12-13 10:49:23 +0300

    Добавлена страница "Контакты"

--- framework/urls.py
+++ framework/urls.py
@@ -1,6 +1,7 @@
-from framework.views import IndexView, AboutView
+from framework.views import IndexView, AboutView, ContactsView
 
 urlpatterns = {
     '/': IndexView(),
     '/about/': AboutView(),
+    '/contacts/': ContactsView(),
 }
