69ea11d8d527e324e052fa10025f2d318f554122
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2022-12-22 10:15:58 +0300

    Декоратор для добавления связки url-view

---
+++ patterns/structural_patterns.py
@@ -0,0 +1,7 @@
+class AppRoute:
+    def __init__(self, routes, url):
+        self.routes = routes
+        self.url = url
+
+    def __call__(self, cls):
+        self.routes[self.url] = cls()
--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from wsgiref.simple_server import make_server
 
 from framework.main import Framework
+from framework.views import routes
 
-application = Framework()
+application = Framework(routes)
 
 with make_server('', 8080, application) as httpd:
     print('Сервер запущен: http://127.0.0.1:8080')
